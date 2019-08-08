5a068a277d25e77337f30a510fc3a5fe2f396e9d
Syed Muhammad Sajjad Ali Zaidi <sajjadalizaidi00@gmail.com>
2019-08-08 12:33:21 +0500

    Individual+All Task(s) Deletion Functionality Added. Filter tasks functionality also added

--- app.js
+++ app.js
@@ -10,17 +10,63 @@ const taskInput = document.querySelector('#task');
 
 LoadEventListeners();
 
-function LoadEventListeners(){
-    form.addEventListener('submit',addTask);
-    
+function LoadEventListeners() {
+    //Add Task Event
+    form.addEventListener('submit', addTask);
+    //Remove task Event
+    taskList.addEventListener('click', removeTask);
+    //Clear task Event
+    clearBtn.addEventListener('click', clearTasks);
+    //Filter Task Event
+    filter.addEventListener('keyup', filterTasks);
 }
 
-function addTask(e){
-    
-    if(taskInput.value === ''){
+function filterTasks(e) {
+    const text = e.target.value.toLowerCase();
+
+    document.querySelectorAll('.collection-item').forEach(function (task) {
+        const item = task.firstChild.textContent;
+        if (item.toLowerCase().indexOf(text) !== -1) {
+            task.style.display = 'block';
+        } else {
+            task.style.display = 'none';
+        }
+
+    })
+
+    console.log(text);
+}
+
+function clearTasks(e) {
+    // first way
+    // taskList.innerHTML = '';
+
+    // 2nd and Faster way
+    if (confirm('Delete all Tasks?')) {
+        while (taskList.firstChild) {
+            taskList.removeChild(taskList.firstChild);
+        }
+    }
+}
+
+function removeTask(e) {
+
+    if (e.target.parentElement.classList.contains('delete-item')) {
+        if (confirm('Are you sure?')) {
+            e.target.parentElement.parentElement.remove();
+        }
+    }
+
+    e.preventDefault();
+}
+
+
+function addTask(e) {
+
+    if (taskInput.value === '') {
         alert('Add a Task');
     }
-    else{
+    else {
         //Create li element
         const li = document.createElement('li');
         //Add class
